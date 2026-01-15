88e99b2c2b7ea9c6bb93fd1f26aca77c5fe4c4c1
noregista <noregista@users.noreply.github.com>
2026-01-15 21:36:02 +0900

    Add Google AdSense verification code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
                 <link rel="apple-touch-icon" href="/images/profile.jpg" />
+                {/* Google AdSense */}
+                <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9663321520769561"
+                    crossOrigin="anonymous"
+                />
             </head>
             <body className="antialiased">
                 {children}
